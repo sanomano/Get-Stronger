1b1b32d1db3c51ceffdd9cfc294d94841fe7111e
sanomano <mirko.kostic@yahoo.com>
2022-07-16 04:08:04 +0200

    new background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import mainLogo from './assets/logo.png'
+import mainLogo from './assets//images/logo.png'
 
 function App() {
   return (
     <div className='app'>
       <div className='header'>
         <img src={mainLogo} alt="logo"></img>
-        <div>
-          <div>Language</div>
-          <div>+</div>
+        <div className='header-right-side'>
+          <div className='header-language'>Language</div>
+          <div className='dropdown-menu'>+</div>
         </div>
       </div>
     </div>
